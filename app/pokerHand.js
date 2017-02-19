3a03c72da37d5b2e2a67a0b41057125445db938c
Paul Monnier <PaulMonnier@Pauls-MacBook-Pro.local>
2017-02-19 11:32:31 +0100

    Begining of code refactorisation

--- app/pokerHand.js
+++ app/pokerHand.js
@@ -1,13 +1,6 @@
 var cardsValues = {};
 
-main();
-
-function main() {
-    console.log('Le jeu de poker commence')
-    initCardsValue();
-    startGame('2H 3D 5S 2C KD', '2C 3H 4S 8C AH');
-    console.log('Fin du jeu');
-}
+var parseHand = hand => hand.split(' ').map(x => [x.charAt(0), x.charAt(1)]);
 
 function initCardsValue() {
     cardsValues['2'] = 2;
@@ -40,82 +33,57 @@ function computeCombination(parsedHand, handNbOccurences) {
     //checkPaire(parsedHand, handNbOccurences);
 }
 
-function parseHand(hand) {
-    var cards = hand.split(' ');
-    var parsedCards = [];
-    for (var card of cards) {
-        var value = card.charAt(0);
-        var color = card.charAt(1);
-        parsedCards.push([value, color])
-    }
-    return parsedCards;
+function countNbOccurencesOfEachCard(hand) {
+    return hand.reduce((acc, [value, color]) => {
+        acc[value] = value in acc ? acc[value] + 1 : 1
+        return acc;
+    }, {})
 }
 
-function countNbOccurencesOfEachCard(firstHand) {
-    var obj = { };
-    for (var i = 0, j = firstHand.length; i < j; i++) {
-        if (obj[firstHand[i][0]])
-            obj[firstHand[i][0]]++;
-        else
-            obj[firstHand[i][0]] = 1;
-    }
-    return obj;
-}
+const valueToString = value => ['2','3','4','5','6','7','8','9','10','J','Q','K','A'][value - 2]
 
-function valueToString(value) {
-    var  cardsValues = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    return cardsValues[value - 2];
-}
+var highestCard = hand => [...hand].sort(([value, color]) => value)[0][0]
 
-function highestCard(parsedHand) {
-    var highestValue = 0;
-    for (var i = 0; i < parsedHand.length; i++) {      
-        if (cardsValues[parsedHand[i][0]] > highestValue)
-            highestValue = cardsValues[parsedHand[i][0]];
-    }
-    return valueToString(highestValue);
-}
-
-function checkPaires(parsedHand, handOccurences) {
+function checkPaires(hand, handOccurences) {
     var paires = [];
-    for (var i = 0; i < parsedHand.length; i++) {
-        if (handOccurences[parsedHand[i][0]] === 2) {
+    for (var i = 0; i < hand.length; i++) {
+        if (handOccurences[hand[i][0]] === 2) {
             if (paires.length === 0)
-                paires.push(parsedHand[i][0]);
-            else if ((paires[paires.length - 1] !== parsedHand[i][0]))
-                paires.push(parsedHand[i][0]);
+                paires.push(hand[i][0]);
+            else if ((paires[paires.length - 1] !== hand[i][0]))
+                paires.push(hand[i][0]);
         }
     }
     return paires;
 }
 
-function threeOfAKind(parsedHand, handOccurences) {
+function threeOfAKind(hand, handOccurences) {
     var threeOfAKind = 0;
-    for (var i = 0; i < parsedHand.length; i++) {
-        if (handOccurences[parsedHand[i][0]] === 3)
-            threeOfAKind = parsedHand[i][0];
+    for (var i = 0; i < hand.length; i++) {
+        if (handOccurences[hand[i][0]] === 3)
+            threeOfAKind = hand[i][0];
     }
     return threeOfAKind;
 }
 
-function straightHand(parsedHand) {
+function straightHand(hand) {
     var consecutiveNb = [];
-    parsedHand.sort();
-    for (var i = 0; i < parsedHand.length - 1; i++) {
-        if (cardsValues[parsedHand[i][0]] + 1 === cardsValues[parsedHand[i + 1][0]]) 
-            consecutiveNb.push(parsedHand[i][0]);
+    hand.sort();
+    for (var i = 0; i < hand.length - 1; i++) {
+        if (cardsValues[hand[i][0]] + 1 === cardsValues[hand[i + 1][0]]) 
+            consecutiveNb.push(hand[i][0]);
     }
-    if (cardsValues[parsedHand[3][0]] + 1 === cardsValues[parsedHand[4][0]])
-        consecutiveNb.push(parsedHand[4][0]);
+    if (cardsValues[hand[3][0]] + 1 === cardsValues[hand[4][0]])
+        consecutiveNb.push(hand[4][0]);
     return consecutiveNb;
 }
 
-function flushHand(parsedHand) {
+function flushHand(hand) {
     var consecutiveColorNb = 0;
-    for (var i = 0; i < parsedHand.length - 1; i++)
-        if (parsedHand[i][1] === parsedHand[i + 1][1])
+    for (var i = 0; i < hand.length - 1; i++)
+        if (hand[i][1] === hand[i + 1][1])
             consecutiveColorNb++;
-    if (parsedHand[3][1] === parsedHand[4][1])
+    if (hand[3][1] === hand[4][1])
         consecutiveColorNb++
     return consecutiveColorNb;
 }
@@ -136,6 +104,15 @@ function fourOfAKind(parsedHand) {
             return parsedHand[i][0];
     return '';
 }
+
+function main() {
+    console.log('Le jeu de poker commence')
+    initCardsValue();
+    startGame('2H 3D 5S 2C KD', '2C 3H 4S 8C AH');
+    console.log('Fin du jeu');
+}
+
+main();
 
 module.exports = {
     startGame: startGame,
